45ad375488a064e53a8533526b8b877b86ef9bbd
martoff <martoff@ukr.net>
2026-03-20 00:07:31 +0200

    'Solution'

--- app/cafe.py
+++ app/cafe.py
@@ -10,7 +10,7 @@ class Cafe:
     def __init__(self, name: str) -> None:
         self.name = name
 
-    def visit_cafe(self, visitor: dict) -> None:
+    def visit_cafe(self, visitor: dict) -> str:
         # Проверка вакцинации
         if "vaccine" not in visitor:
             raise NotVaccinatedError("Visitor is not vaccinated")
